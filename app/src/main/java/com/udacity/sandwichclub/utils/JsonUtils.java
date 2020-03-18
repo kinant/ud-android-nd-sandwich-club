8edb2fd547958979b0ba6b83aa008297af489044
Kinan T <kinan.t@gmail.com>
2020-03-17 22:30:15 -0600

    JSON Parsing 1
    Correctly get's the whole sandwich json object. Will not work on getting each of the individual elements.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,26 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
+
+        Log.d("JSON", "in json function!");
+
+        try {
+            // Get the sandwich JSON Object
+            JSONObject sandwich = new JSONObject(json);
+            Log.d("JSON 1:", sandwich.toString());
+        } catch(JSONException e){
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
