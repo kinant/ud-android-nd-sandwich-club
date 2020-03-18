99fc3827fa4eae4ddddb0c117c16c187f9e811d8
Kinan T <kinan.t@gmail.com>
2020-03-17 23:18:07 -0600

    Layout 2
    Populates layout with all the details. Will now make it prettier.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,12 +16,24 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    // for populating UI
+    TextView mOrigin;
+    TextView mKnownAs;
+    TextView mIngredients;
+    TextView mDescription;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
+
+        // Use findViewById to get reference to our textviews from the xml
+        mOrigin = findViewById(R.id.origin_tv);
+        mKnownAs = findViewById(R.id.also_known_tv);
+        mIngredients = findViewById(R.id.ingredients_tv);
+        mDescription = findViewById(R.id.description_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,7 +56,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +69,18 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        // Populate UI Text Views with sandwich details
+        mOrigin.setText(sandwich.getPlaceOfOrigin());
+        mDescription.setText(sandwich.getDescription());
 
+        // iterate over the lists
+        for(String knownAs: sandwich.getAlsoKnownAs()){
+            mKnownAs.append((knownAs) + "\n");
+        }
+
+        for(String ing: sandwich.getIngredients()){
+            mKnownAs.append((ing) + "\n");
+        }
     }
 }
