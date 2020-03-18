4e370489b58d91c987d03d6fd43fed6272267af9
Kinan T <kinan.t@gmail.com>
2020-03-17 22:41:17 -0600

    JSON Parsing 2
    Successfully gets all the elements. Will now attempt to convert JSON arrays into strings.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -17,6 +18,32 @@ public class JsonUtils {
             // Get the sandwich JSON Object
             JSONObject sandwich = new JSONObject(json);
             Log.d("JSON 1:", sandwich.toString());
+
+            // get the main name
+            JSONObject name = sandwich.getJSONObject("name");
+            String mainName = name.getString("mainName");
+            Log.d("JSON mainName:", mainName);
+
+            // get the also known as array
+            JSONArray knownAs = name.getJSONArray("alsoKnownAs");
+            Log.d("JSON knownAs:", knownAs.toString());
+
+            // get the place of origin
+            String origin = sandwich.getString("placeOfOrigin");
+            Log.d("JSON origin:", origin);
+
+            // get the description
+            String desc = sandwich.getString("description");
+            Log.d("JSON desc:", desc);
+
+            // get the image url
+            String imgUrl = sandwich.getString("image");
+            Log.d("JSON img:", imgUrl);
+
+            // get the ingredients array
+            JSONArray ingredients = sandwich.getJSONArray("ingredients");
+            Log.d("JSON ingredients:", ingredients.toString());
+
         } catch(JSONException e){
             e.printStackTrace();
         }
